b80a8c71f0850c669b159d7c3616886647f9b3ca
Pham Duc <phamduc0701@gmail.com>
2020-10-15 16:44:17 +0700

    + Tests/ folder, reconstruct project tree

--- Src/sample.cpp
+++ Src/sample.cpp
@@ -13,10 +13,43 @@ int add(int a, int b)
 	return a + b*2;
 }
 
+int mix(int a, int b, int c)
+{
+	return a + b * c;
+}
+
+
+struct Pet
+{
+public:
+	int value_public;
+	Pet(const std::string &name): name(name){}
+	void setName(const std::string &name){this->name = name;}
+	const std::string& getName() const {return name;}
+	const int& getValue() const {return value;}
+private:
+	std::string name;
+	int value = 223;
+};
+
 PYBIND11_MODULE(sample, m)
 {
 	m.doc() = "Sample for PyBind11";
-	m.def("subtract", &subtract, "A subtract function");
+	py::object name =  py::cast("Pham Trung Duc");
+	m.attr("NAME") = name;
+	m.attr("PI") = 3.14159265;
+	m.def("subtract", &subtract, "A subtract function", py::arg("a") = 3, py::arg("b") = 100);
 	m.def("add", &add, "A sum function");
+
+	using namespace pybind11::literals; // Using namespace Literals to shorten the syntax
+	m.def("mixture", &mix, "A mix function", "a"_a, "b"_a, "c"_a=24);
+
+	py::class_<Pet>(m, "Pet", py::dynamic_attr()) // Dynamci flags to use dynamic attribute
+		.def(py::init<const std::string&>())
+		.def("setName", &Pet::setName)
+		.def("getName", &Pet::getName)
+		.def_property_readonly("value", &Pet::getValue)
+		.def("__repr__",[](const Pet& pet){return "<sample.Pet named " + pet.getName() + ">";})
+		.def_readwrite("value", &Pet::value_public); // Attribute as property 
 }
 
--- Test.py
+++
@@ -1,2 +0,0 @@
-import sample
-print(sample.add(2412345, 235))
