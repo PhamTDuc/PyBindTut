a41d8f000a044ef75d1fdccdb49aa7ace7b8ee01
Pham Duc <phamduc0701@gmail.com>
2020-10-21 16:57:11 +0700

    - Custom constructor basic DONE

--- Src/custom_constructor05.cpp
+++ Src/custom_constructor05.cpp
@@ -9,6 +9,11 @@ private:
 	Example(int value):data(value){}
 public:
 	static Example create(int value){return Example(value);}
+	Example(double){py::print("Example argument with double");}; 
+	Example(int, int){py::print("Example argument with 2 integers");};
+	Example(std::string&);
+
+
 	int get() const {return data;}
 };
 
@@ -18,5 +23,6 @@ PYBIND11_MODULE(custom_constructor, m)
 	m.doc() = "Custom Constructor demo";
 	py::class_<Example>(m, "Example")
 		.def(py::init(&Example::create), py::arg() = 0)
+		.def(py::init([](std::string&){py::print("Example argument with String"); return Example::create(0);}))
 		.def("get", &Example::get);
 }
--- Tests/Test_custom_constructor.py
+++ Tests/Test_custom_constructor.py
@@ -1,5 +1,7 @@
-from custom_construct import Example
+from custom_constructor import Example
 
 
-example = Example()
+example = Example(521)
 print(example.get())
+example_02 = Example("Hello the world")
+print(example_02.get())
